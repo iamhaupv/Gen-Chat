9175cdfffd46d64f77aecd185a28f246fc3883f1
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-04-09 15:43:26 +0700

    find room by phoneNumber successfully

--- nodejs-genchat/src/repositories/roomRepository.js
+++ nodejs-genchat/src/repositories/roomRepository.js
@@ -10,7 +10,20 @@ const createRoom = async(users, relationship)=>{
         throw new Error(error)
     }
 }
-
+// findRoomByPhoneNumber
+const findRoomByPhoneNumber = async(phoneNumber) =>{
+    try {
+        const room = Room.findOne({phoneNumber})
+        if(!room){
+            throw new Error("Room is not exist!")
+        }
+        return room;
+    } catch (error) {
+        console.log(error)
+        throw new Error(error)
+    }
+}
 module.exports = {
+    findRoomByPhoneNumber,
     createRoom
 }
--- nodejs-genchat/src/routes/roomRouter.js
+++ nodejs-genchat/src/routes/roomRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const {roomController} = require("../controllers/index")
 // createRoom
 router.post("/createRoom", roomController.createRoom)
-
+// findRoomByPhoneNumber
+router.post("/findRoomByPhoneNumber", roomController.findRoomByPhoneNumber)
 module.exports = router;
