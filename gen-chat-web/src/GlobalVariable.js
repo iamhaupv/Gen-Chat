659606c0c301483185d48d6ca60a501fa1008fa1
LeeTrongjNghiax <leetrongjnghiax0938225745@gmail.com>
2024-04-14 14:23:08 +0700

    web updated gitignore again

--- gen-chat-web/src/GlobalVariable.js
+++ gen-chat-web/src/GlobalVariable.js
@@ -1,6 +1,6 @@
 // const api_host = " https://16e2-27-64-209-107.ngrok-free.app"
 const api_host = " https://localhost:6969"
 // const socket_host = "http://0.tcp.ap.ngrok.io:16233" // tcp trong url doi thanh http nha
-const socket_host = "http://localhost:6969" // tcp trong url doi thanh http nha
+const socket_host = "http://localhost:6969"
 
 export default {api_host, socket_host};
