5f4de90a5c4668c6ded94d2ad06f6b8afa85dbf0
NguyenThanhKhoa <nguyenthanhkhoa8888@gmail.com>
2024-04-15 11:31:34 +0700

    web added file added

--- gen-chat-web/src/components/ChatInput.js
+++ gen-chat-web/src/components/ChatInput.js
@@ -3,6 +3,10 @@ import React, {useState, useRef, useEffect} from 'react';
 export default function ChatInput({socketRef, user}) {
   const [message, setMessage] = useState('');
 
+  const openInputFile = () => {
+    document.getElementById("file").click();
+  }
+
   const handleMessage = event => {
     setMessage(event.target.value);
   }
@@ -31,9 +35,12 @@ export default function ChatInput({socketRef, user}) {
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
         <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
       </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6"
+        onClick={openInputFile}
+      >
         <path strokeLinecap="round" strokeLinejoin="round" d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13" />
       </svg>
+      <input type='file' id='file' className='hidden'></input>
       <input type='text' id='message' placeholder='Type your message...' className='border-2 rounded-full p-2 grow'
         values={message}
         onChange={handleMessage}
