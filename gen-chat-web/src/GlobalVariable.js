803d6a54af6af25e7d10aa6baf53fa5e5c232f38
NguyenThanhKhoa <nguyenthanhkhoa8888@gmail.com>
2024-04-17 10:50:45 +0700

    web added new api

--- gen-chat-web/src/GlobalVariable.js
+++ gen-chat-web/src/GlobalVariable.js
@@ -1,6 +1,7 @@
 // const api_host = " https://16e2-27-64-209-107.ngrok-free.app"
 const api_host = " http://localhost:6969"
 // const socket_host = "http://0.tcp.ap.ngrok.io:16233" // tcp trong url doi thanh http nha
-const socket_host = "http://localhost:6969"
+const socket_host = "http://localhost:6969" //chat 1-1
+const socket_host_Group = "http://localhost:7000"//chat group
 
 export default {api_host, socket_host};
