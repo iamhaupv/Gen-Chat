a67d9833c6cd6980e5f891157ba9148b42c56748
NguyenThanhKhoa <nguyenthanhkhoa8888@gmail.com>
2024-04-26 17:01:11 +0700

    added new mobile project 2

---
+++ MobileGenChat_2/pages/Home.js
@@ -0,0 +1,37 @@
+import {
+  Box, 
+  Text 
+} from "@gluestack-ui/themed"
+  
+import { createProvider } from "@gluestack-ui/provider"
+import { StyledProvider } from "@gluestack-style/react"
+import { config } from "@gluestack-ui/config"
+  
+import React from 'react'
+import { Pressable, View } from 'react-native'
+  
+export const Provider = createProvider({
+    StyledProvider,
+})
+  
+export default function Home({navigation}) {
+  return (
+    <Provider config={config}>
+      <View>
+        <Text>lmao</Text>
+  
+        <Box bg="$primary500" p="$1">
+          <Text color="white">This is the Box</Text>
+        </Box>
+
+        <Pressable
+          title="Go to Details"
+          onPress={() => navigation.navigate('Main')}
+        >
+          <Text>Ve main</Text>
+        </Pressable>
+      </View>
+    </Provider>
+  )
+}
+  
