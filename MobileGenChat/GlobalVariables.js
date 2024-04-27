401e85fa6cebee95d2bfdaddad97e0c759a8e2da
NguyenThanhKhoa <nguyenthanhkhoa8888@gmail.com>
2024-04-27 13:55:42 +0700

    Test apk and ngrok finish

--- MobileGenChat/GlobalVariables.js
+++ MobileGenChat/GlobalVariables.js
@@ -1,4 +1,4 @@
-// const api_host = "https://9830-2001-ee0-50c4-3bf0-bc20-b6e2-e6f6-c59d.ngrok-free.app"
+// const api_host = "https://42bc-2001-ee0-50c4-3bf0-5992-9052-5302-eb7.ngrok-free.app" sử dụng dc
 const api_host = "http://10.0.2.2:6969"
 // const socket_host = "tcp://0.tcp.ap.ngrok.io:17286"
 const socket_host = "http://10.0.2.2:7000"
