eb45697e92fa78a6529584afd2545a0cbd057b31
NguyenThanhKhoa <nguyenthanhkhoa8888@gmail.com>
2024-05-23 14:16:52 +0700

    web updated config file

--- gen-chat-web/src/firebase/config.js
+++ gen-chat-web/src/firebase/config.js
@@ -4,7 +4,10 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
+// Nghiaxth2
+const firebaseConfig = {
+  
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
